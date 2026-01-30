a7d76e5ed08f81ece867dab10f28d499cabb95fc
karamihaii <erikara2223@gmail.com>
2026-01-30 12:53:26 -0600

    tried to get refresh to work

--- BrandLogoApp/app/(tabs)/customize.tsx
+++ BrandLogoApp/app/(tabs)/customize.tsx
@@ -1,10 +1,69 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { useAuth } from "@/components/AuthProvider";
+import { supabase } from "@/utils/supabase";
+import React, { useCallback, useEffect, useState } from "react";
+import { RefreshControl, ScrollView, Text } from "react-native";
 import defaultStyles from "../../styles/defaultStyles";
 export default function FamilyScreen() {
+  const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [refreshing, setRefreshing] = useState(false);
+    const [skiOrBoard, setSkiOrBoard] = useState("");
+  const { session } = useAuth();
+
+
+useEffect(() => {
+  async function loadProfile() {
+    if (!session?.user) return;
+
+    const { data, error } = await supabase
+      .from("profiles")
+      .select("first_name, last_name, ski_or_board")
+      .eq("id", session.user.id)
+      .single();
+
+    if (!error && data) {
+      setFirstName(data.first_name);
+      setLastName(data.last_name);
+      setSkiOrBoard(data.ski_or_board);
+    }
+  }
+
+  loadProfile();
+}, [session]);
+
+const onRefresh = useCallback(async () => {
+    setRefreshing(true);
+    if (!session?.user) return;
+
+    const { data, error } = await supabase
+      .from("profiles")
+      .select("first_name, last_name, ski_or_board")
+      .eq("id", session.user.id)
+      .single();
+
+    if (!error && data) {
+      setFirstName(data.first_name);
+      setLastName(data.last_name);
+      setSkiOrBoard(data.ski_or_board);
+    }
+    setRefreshing(false);
+  }, [session]);
   return (
-    <View style={defaultStyles.pageContainer}>
-      <Text style={defaultStyles.bodyText}>Customize</Text>
-    </View>
+    <ScrollView
+      contentContainerStyle={defaultStyles.pageContainer}
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }
+    >
+      <Text style={defaultStyles.bodyText}>
+        Customize your coolest {skiOrBoard}
+      </Text>
+    </ScrollView>
   );
 }
+
+
+function loadProfile() {
+  throw new Error("Function not implemented.");
+}
+
